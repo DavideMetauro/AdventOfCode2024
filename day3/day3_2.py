18c1d3bc626821ab5cd1af41b08c44dc9a948738
Davide Metauro <125986164+DavideMetauro@users.noreply.github.com>
2024-12-03 18:33:21 +0000

    day3 part 2

---
+++ day3/day3_2.py
@@ -0,0 +1,33 @@
+import re
+
+def read_input():
+    with open("input3.txt") as f:
+        return f.read().replace('\n','*')
+    
+def find_instructions(data):
+    instructions = re.findall(r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)',data)
+    #values = re.findall(r'do\(\)|don\'t\(\)',data)
+
+    return instructions
+
+def calculate_mul(instructions):
+    sum=0
+    enebled = True
+    for instr in instructions:
+        if instr == 'do()':
+            enebled = True
+        elif instr == 'don\'t()':
+            enebled = False
+        elif enebled:
+            values = re.findall(r'\d+',instr)
+            x, y = map(int,values)
+            sum+=x*y
+    return sum
+    
+def main():
+    data = read_input()
+    instructions=find_instructions(data)
+    print(calculate_mul(instructions))
+
+if __name__ == '__main__':
+    main()
