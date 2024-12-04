f5b80c703df7da08e881542e02d167a8d9e0a1ae
Davide <davide.metauro@mail.polimi.it>
2024-12-04 09:13:15 +0100

    diagonal

--- day4/day4.py
+++ day4/day4.py
@@ -13,14 +13,30 @@ def findXmas(strings):
         matches.append(re.findall(r'XMAS|SAMX',line))
     return matches
 
+def flatten_diagonally(matrix):
+    diagonals = []
+    for i in range(len(matrix) + len(matrix[0]) - 1):
+        diagonal = []
+        for j in range(max(0, i - len(matrix[0]) + 1), min(len(matrix), i + 1)):
+            diagonal.append(matrix[j][i - j])
+        diagonals.append(''.join(diagonal))
+    return diagonals
+
+
 def main():
     count=0
     lines = read_input()
+    matrix = listToMatrix(lines)
     count+=len(findXmas(lines))
     print(count)
-    column_strings = [''.join(row) for row in zip(*listToMatrix(lines))]
+    
+    column_strings = [''.join(row) for row in zip(*matrix)]
     #print(column_strings)
     count+=len(findXmas(column_strings))
+    print(count)
+    
+    diagonal_strings = flatten_diagonally(matrix)
+    count += len(findXmas(diagonal_strings))
     print(count)
     #print(listToMatrix(lines))
     
