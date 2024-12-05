a70d3ca753a4b1c1b4be6e1aba35711001db9dd9
Davide <davide.metauro@mail.polimi.it>
2024-12-05 14:54:11 +0100

    divided into functions

--- day5/day5.py
+++ day5/day5.py
@@ -2,18 +2,30 @@
 #setY
 #map(num, countNum)
 #index[len(setX)]
-with open('input5.txt', 'r') as file:
-    input_data = file.read().split('\n\n')
-    part1 = input_data[0].strip().split()
-    part2 = input_data[1].strip().split(',')
+def read_input():
+    with open('input5.txt', 'r') as file:
+        input_data = file.read().split('\n\n')
+        part1 = input_data[0].strip().split()
+        part2 = input_data[1].strip().split(',')
+    return part1, part2
 
-setX = set()
-setY = set()
-map_num_count = {}
+def create_sets(part1):
+    setX = set()
+    setY = set()
 
-for pair in part1:
-    num1, num2 = map(int, pair.split('|'))
-    setX.add(num1)
-    setY.add(num2)
-    
-print(setX)
+    for pair in part1:
+        num1, num2 = map(int, pair.split('|'))
+        setX.add(num1)
+        setY.add(num2)
+        
+    print(setX)
+    print(setY)
+    return setX, setY
+
+def create_map(part2, setX, setY):
+    map_num_count = {}
+
+if __name__ == '__main__':
+    part1, part2 = read_input()
+    setX, setY = create_sets(part1)
+    create_map(part2, setX, setY)
