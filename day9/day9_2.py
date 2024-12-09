1543f5def926615f44178c062f3c66c612747f41
DavideMetauro <davide.metauro@mail.polimi.it>
2024-12-09 23:18:40 +0100

    day9 part 2 failed, restored

--- day9/day9_2.py
+++ day9/day9_2.py
@@ -17,14 +17,18 @@ def read_input():
             count+=1
 
 def rearrange():
-    
+    last_index = len(quantity)-1
+    for i in range(len(quantity)):
+        for j in range(clear[i]):
+            final.append(last_index)
+            quantity[last_index] -= 1
+            if quantity[last_index] == 0:
+                last_index -= 1
 
 def create_final():
     for i in range(len(quantity)-1):
-        for j in range(quantity[i]):
-            final.append(i)
-        for j in range(clear[i]):
-            final.append(None)
+        final.extend([i for j in range(quantity[i])])
+        final.extend([None for j in range(clear[i])])
     for j in range(quantity[-1]):
         final.append(len(quantity)-1)
 
