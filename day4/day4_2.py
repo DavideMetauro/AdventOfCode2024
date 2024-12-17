dc2751f184d64c0e2dc99fc89b2fbdd2806d0569
Davide Metauro <125986164+DavideMetauro@users.noreply.github.com>
2024-12-17 16:28:31 +0000

    part 2 almost finished

---
+++ day4/day4_2.py
@@ -0,0 +1,26 @@
+import re
+
+def read_input():
+    with open('input4.txt') as f:
+        return f.readlines()
+
+def listToMatrix(lines):
+    return [list(line.strip()) for line in lines]
+
+def findX_Mas(matrix):
+    matches=[]
+    for i in range(1,len(matrix)-1):
+        for j in range(1,len(matrix[0])-1):
+            row = ''.join(matrix[i-1][j-1], matrix[i][j], matrix[i+1][j+1])
+            row2 = ''.join(matrix[i-1][j+1], matrix[i][j], matrix[i+1][j-1])
+            if (row == 'MAS' or row == 'SAM') and (row2 == 'MAS'  or row2 == 'SAM'):
+                matches.append(i)
+    return matches
+
+def main():
+    lines = read_input()
+    matrix = listToMatrix(lines)
+    print(len(findX_Mas(matrix)))
+
+if __name__ == '__main__':
+    main()
