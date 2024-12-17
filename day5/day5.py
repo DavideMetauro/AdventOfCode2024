7bca20ec8b4633a482ac5bd3b97852877e3bf411
DavideMetauro <davide.metauro@mail.polimi.it>
2024-12-17 23:37:02 +0100

    day5 solved with comparator

--- day5/day5.py
+++ day5/day5.py
@@ -1,46 +1,41 @@
-#setX
-#setY
-#map(num, countNum)
-#index[len(setX)]
+from functools import cmp_to_key
+
+comparison = {}
+
 def read_input():
     with open('input5.txt', 'r') as file:
         input_data = file.read().split('\n\n')
         part1 = input_data[0].strip().split()
-        part2 = input_data[1].strip().split(',')
+        part2 = input_data[1].strip().split()
     return part1, part2
 
 def create_comparison(part1):
-    comparison = {}
     for pair in part1:
         num1, num2 = map(int, pair.split('|'))
         if num1 not in comparison:
             comparison[num1] = []
         comparison[num1].append(num2)
-    return comparison
 
-#return true if num1 is bigger than num2. Default is false (num1 is smaller than num2)
-def compare(comparison, num1, num2):
-    if num1 not in comparison:
-        return False
-    return True if num2 in comparison[num1] else False
+#return true if num1 is bigger (printed before) than num2. Default is false (num1 is smaller than num2)
+def compare(num1, num2):
+    if num1 == num2 :
+        return 0
+    return 1 if num1 in comparison and num2 in comparison[num1] else -1
 
-'''
-def create_sets(part1):
-    setX = set()
-    setY = set()
-
-    for pair in part1:
-        num1, num2 = map(int, pair.split('|'))
-        setX.add(num1)
-        setY.add(num2)
-        
-    print(setX)
-    print(setY)
-    return setX, setY
-'''
+def main():
+    correct = 0
+    wrong = 0
+    part1, part2 = read_input()
+    create_comparison(part1)
+    for line in part2:
+        num= list(map(int, line.split(',')))
+        num_sorted = sorted(num, key=cmp_to_key(compare), reverse=True)
+        if num == num_sorted:
+            correct += num[len(num) // 2 ]
+        else:
+            wrong += num_sorted[len(num_sorted) // 2]
+    print("part1: ",correct)
+    print("part2: ",wrong)
 
 if __name__ == '__main__':
-    part1, part2 = read_input()
-    print(create_comparison(part1))
-
-    
+    main()   
