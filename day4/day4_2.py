ed279ce7e5f8f81149cc3d1f697e70ed0bcc84c5
Davide Metauro <125986164+DavideMetauro@users.noreply.github.com>
2024-12-17 16:38:32 +0000

    part 2 solved

--- day4/day4_2.py
+++ day4/day4_2.py
@@ -11,8 +11,10 @@ def findX_Mas(matrix):
     matches=[]
     for i in range(1,len(matrix)-1):
         for j in range(1,len(matrix[0])-1):
-            row = ''.join(matrix[i-1][j-1], matrix[i][j], matrix[i+1][j+1])
-            row2 = ''.join(matrix[i-1][j+1], matrix[i][j], matrix[i+1][j-1])
+            row = matrix[i-1][j-1] + matrix[i][j] + matrix[i+1][j+1]
+            row2 = matrix[i-1][j+1] + matrix[i][j] + matrix[i+1][j-1]
+            print(row)
+            print(row2)
             if (row == 'MAS' or row == 'SAM') and (row2 == 'MAS'  or row2 == 'SAM'):
                 matches.append(i)
     return matches
