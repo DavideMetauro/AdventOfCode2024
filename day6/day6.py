90ab6815c7497c0cc0b3c8c3ad201d76f069e03b
Davide Metauro <125986164+DavideMetauro@users.noreply.github.com>
2024-12-18 20:26:07 +0000

    iterative solution

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,5 @@
 directions = ['^', '>', 'v', '<']
+matrix=[]
 def read_input():
     with open("input6.txt") as f:
         return [line.strip() for line in f.readlines()]
@@ -9,8 +10,8 @@ def find_start(lines):
             if char in ['^', '<', '>', 'v']:
                 return (i, j, char)
             
-def move(x, y, direction, matrix):
-    
+def move(x, y, direction):
+
     if direction == '^':
         x_new, y_new=x-1, y
     elif direction == '<':
@@ -22,22 +23,22 @@ def move(x, y, direction, matrix):
     
     if not(x_new<0 or x_new>=len(matrix) or y_new<0 or y_new>=len(matrix[0])):
         if matrix[x_new][y_new] == '#':
-            move( x, y, directions[(directions.index(direction)+1)%4], matrix)
+            return x, y, directions[(directions.index(direction)+1)%4]
         else:
             matrix[x_new][y_new] = 'X'
-            move( x_new, y_new, direction, matrix)
-        return
+            return x_new, y_new, direction
     else: 
-        return 
+        return None, None, None
     
 def main():
     lines = read_input()
+    global matrix
     matrix = [list(line.strip()) for line in lines]
     x, y, dir = find_start(matrix)
     matrix[x][y] = 'X'
-    move(x, y, dir, matrix)
+    while x is not None or y is not None or dir is not None:
+        x,y,dir=move(x, y, dir)
     count_x = sum(line.count('X') for line in matrix)
-    print("\n".join("".join(line) for line in matrix))
     print(f"Total X count: {count_x}")
 
 if __name__ == "__main__":
