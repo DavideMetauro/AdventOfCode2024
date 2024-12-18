da3ef3ff24fef10f99886c5ec0ac5e4646bad388
Davide Metauro <125986164+DavideMetauro@users.noreply.github.com>
2024-12-18 14:26:24 +0000

    it turns after an obstacle

--- day6/day6.py
+++ day6/day6.py
@@ -8,26 +8,30 @@ def find_start(lines):
             if char in ['^', '<', '>', 'v']:
                 return (i, j, char)
             
-def move(x, y, direction, lines):
+def move(x, y, direction, matrix):
     directions = ['^', '>', 'v', '<']
-    if lines[x][y]=='#':
+    if matrix[x][y]=='#':
         direction = directions[(directions.index(direction)+1)%4]
     else:
-        lines[x][y] = 'X'
+        matrix[x][y] = 'X'
     if direction == '^':
-        return (x-1, y)
+        return (x-1, y, direction)
     elif direction == '<':
-        return (x, y-1)
+        return (x, y-1, direction)
     elif direction == '>':
-        return (x, y+1)
+        return (x, y+1, direction)
     elif direction == 'v':
-        return (x+1, y)
+        return (x+1, y, direction)
     
 def main():
     lines = read_input()
+    matrix = [list(line.strip()) for line in lines]
     directions = ['^', '<', '>', 'v']
-    print(lines)
-    startX, startY, startDir = find_start(lines)
+    x, y, dir = find_start(matrix)
+    while x>=0 and x<len(matrix) and y>=0 and y<len(matrix[0]):
+        x, y, dir = move(x, y, dir, matrix)
+    count_x = sum(line.count('X') for line in matrix)
+    print(f"Total X count: {count_x}")
 
 if __name__ == "__main__":
     main()
